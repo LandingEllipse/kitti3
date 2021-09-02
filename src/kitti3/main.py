3b5a29521a88676c1199ed9167ba6fd1292ab127
Ariel Ladegaard <legendin@gmail.com>
2021-09-02 01:28:34 +0100

    refactor: split i3 and sway specific functionality
    Let new Kitt base class handle event handler guarding and such, while
    delegating the actual alignment and fetching to the Kitti3 and Kitts
    subclasses. Also allows e.g. the Kitts subclass to prepend some commands
    to the spawn method.
    
    Factor out the actual rect calculation from the align_to_ws method, and
    provisionally sneak in an LRU cache (need to test whether the cache
    access is actually any faster than just doing the rect calculations...)
    
    The seeds for generalising Kitty are sown in this refactor, but front-
    end support for client specification will be implemented separately.
    
    Mainline crude 20ms command delay for on_floated under sway, as OoO
    issue described in #17 was regularly observed also for Kitty.

--- src/kitti3/main.py
+++ src/kitti3/main.py
@@ -2,8 +2,11 @@
 
 import argparse
 import enum
+import functools
 import sys
-from typing import Tuple, List, Optional
+import time
+from collections import namedtuple
+from typing import List, Optional, Tuple
 
 import i3ipc
 
@@ -17,6 +20,19 @@ DEFAULTS = {
     "name": "kitti3",
     "shape": (1.0, 0.4),
     "position": "RIGHT",
+}
+
+CLIENTS = {
+    "kitty": {
+        "i3": {
+            "cmd": "--no-startup-id kitty --name {}",
+            "crit_attr": "window_instance",
+        },
+        "sway": {
+            "cmd": "kitty --class {}",
+            "crit_attr": "app_id",
+        },
+    },
 }
 
 
@@ -57,34 +73,57 @@ class Position(enum.Enum):
 
 
 class Shape:
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float):
         if max(x, y) > 1.0 or min(x, y) < 0.0:
             raise ValueError(f"shape out of range [0,1]: x={x}, y={y}")
-        self.x = x
-        self.y = y
+        self.x: float = x
+        self.y: float = y
 
 
-class Kitti3:
-    def __init__(self, name: str, shape: Shape, pos: Position, kitty_argv: list = None):
+Client = namedtuple("Client", ["cmd", "crit_attr"])
+
+
+class Trigger(enum.Enum):
+    KEYBIND = enum.auto()
+    SPAWNED = enum.auto()
+    FLOATED = enum.auto()
+    MOVED = enum.auto()
+
+
+class Rect(namedtuple("Rect", ["x", "y", "w", "h"])):
+    __slots__ = ()
+
+    @classmethod
+    def from_i3ipc(cls, r: i3ipc.Rect):
+        return cls(r.x, r.y, r.width, r.height)
+
+
+class Kitt:
+    def __init__(
+        self,
+        conn: i3ipc.Connection,
+        name: str,
+        shape: Shape,
+        pos: Position,
+        client: Client,
+        client_argv: List[str] = None,
+    ):
+        self.i3 = conn
         self.name = name
         self.shape = shape
         self.pos = pos
-        self.kitty_argv = kitty_argv
+        self.client = client
+        self.client_argv = client_argv
 
-        self.con_id = None
-        self.con_ws = None
-        self.focused_ws = None
+        self.con_id: Optional[str] = None
+        self.con_ws: Optional[i3ipc.WorkspaceReply] = None
+        self.focused_ws: Optional[i3ipc.WorkspaceReply] = None
 
-        self.i3 = i3ipc.Connection()
         self.i3.on("binding", self.on_keybind)
         self.i3.on("window::new", self.on_spawned)
         self.i3.on("window::floating", self.on_floated)
         self.i3.on("window::move", self.on_moved)
         self.i3.on("shutdown::exit", self.on_shutdown)
-
-        # FIXME: half-baked way of checking what WM we're running on.
-        self.sway = "sway" in self.i3.socket_path or self.i3.get_version().major < 3
-        self.align_to_ws = self._align_to_ws_sway if self.sway else self._align_to_ws_i3
 
     def loop(self) -> None:
         """Enter listening mode, awaiting IPC events."""
@@ -109,7 +148,7 @@ class Kitti3:
         elif self.con_ws.name == self.focused_ws.name:
             self.i3.command(f"[con_id={self.con_id}] floating enable, move scratchpad")
         else:
-            self.align_to_ws(fetch=True)
+            self.align_to_ws(Trigger.KEYBIND)
 
     def on_spawned(self, _, we: i3ipc.WindowEvent) -> None:
         """Float the Kitty window once it has settled after spawning.
@@ -118,11 +157,11 @@ class Kitti3:
         alignment.
         """
         con = we.container
-        if self.name not in (con.window_instance, con.app_id):
+        if getattr(con, self.client.crit_attr) != self.name:
             return
         self.con_id = con.id
         self._refresh()
-        self.i3.command(f"[con_id={self.con_id}] floating enable, border none")
+        self.align_to_ws(Trigger.SPAWNED)
 
     def on_floated(self, _, we: i3ipc.WindowEvent) -> None:
         """Ensure that the Kitty window is aligned to its workspace when transitioning
@@ -140,7 +179,7 @@ class Kitti3:
         # despawn guard + toggle-while-tiled trigger repression
         if self.con_ws is None or self.con_ws.name == "__i3_scratch":
             return
-        self.align_to_ws(fetch=False)
+        self.align_to_ws(Trigger.FLOATED)
 
     def on_moved(self, _, we: i3ipc.WindowEvent) -> None:
         """Ensure that the Kitty window is positioned and resized when moved to a
@@ -149,8 +188,7 @@ class Kitti3:
         If Kitty has been manually tiled by the user it will not be re-floated.
         """
         con = we.container
-        # skip if sway until compatibility issue is resolved
-        if self.sway or not (
+        if not (
             # ignore tiled cons
             con.type == "floating_con"
             # event's con is floating wrapper for i3, but target con for sway
@@ -167,89 +205,50 @@ class Kitti3:
             or self.con_ws.name == "__i3_scratch"
         ):
             return
-        self.align_to_ws(fetch=False)
+        self.align_to_ws(Trigger.MOVED)
 
     @staticmethod
     def on_shutdown(_, se: i3ipc.ShutdownEvent):
         exit(0)
 
     def spawn(self) -> None:
-        """Spawn a new Kitty window associated with the name of this Kitti3 instance."""
-        arg = "class" if self.sway else "name"
-        cmd_base = f"exec --no-startup-id kitty --{arg} {self.name}"
-        if self.kitty_argv is None:
-            cmd = cmd_base
-        else:
-            argv = " ".join(self.kitty_argv)
-            cmd = f"{cmd_base} {argv}"
+        """Spawn a new client window associated with the name of this Kitti3 instance."""
+        cmd = f"exec {self.client.cmd.format(self.name)}"
+        if self.client_argv:
+            cmd = f"{cmd} {' '.join(self.client_argv)}"
         self.i3.command(cmd)
 
-    def _align_to_ws_i3(self, fetch: bool = True) -> None:
-        """Adapt the dimensions and location of Kitty's window to the `ws` workspace.
-
-        If `fetch` is True, Kitty will be moved from its current workspace to the
-        focused workspace via the scratchpad (where it might already reside).
-
-        TODO:
-            - We can skip alignment if src and dest WSs are on the same output, and
-              kitty has not gone from tiled to floating (i.e. this method was not called
-              from `on_float()`). However, it's probably not worth the hassle to track.
-        """
-        if self.con_id is None:
-            raise RuntimeError("internal error: Kitty instance ID not yet assigned")
-        ws = self.focused_ws if fetch else self.con_ws
-        width = round(ws.rect.width * self.shape.x)
-        height = round(ws.rect.height * self.shape.y)
-        x = {
-            "L": ws.rect.x,
-            "C": ws.rect.x + round((ws.rect.width / 2) - (width / 2)),
-            "R": ws.rect.x + ws.rect.width - width,
-        }[self.pos.x]
-        y = {
-            "T": ws.rect.y,
-            "C": ws.rect.y + round((ws.rect.height / 2) - (height / 2)),
-            "B": ws.rect.y + ws.rect.height - height,
-        }[self.pos.y]
-
-        self.i3.command(
-            f"[con_id={self.con_id}]"
-            f"{f' move container to workspace {ws.name},' if fetch else ''}"
-            f" resize set {width}px {height}px,"
-            f" move absolute position {x}px {y}px,"
-            f"{ 'focus' if fetch else ''}"
-        )
-
-    def _align_to_ws_sway(self, fetch: bool = True) -> None:
-        """Adapt the dimensions and location of Kitty's window to the `ws` workspace.
-
-        If `fetch` is True, Kitty will be moved from its current workspace to the
-        focused workspace.
-        """
-        if self.con_id is None:
-            raise RuntimeError("internal error: Kitty instance ID not yet assigned")
-        ws = self.focused_ws if fetch else self.con_ws
-
-        width = round(self.shape.x * 100)
-        height = round(self.shape.y * 100)
-
-        x = {
-            "L": 0,
-            "C": round(50 - (width / 2)),
-            "R": 100 - width,
-        }[self.pos.x]
-        y = {
-            "T": 0,
-            "C": round(50 - (height / 2)),
-            "B": 100 - height,
-        }[self.pos.y]
-
-        self.i3.command(
-            f"[con_id={self.con_id}]"
-            f"{f' move container to workspace {ws.name},' if fetch else ''}"
-            f" resize set {width}ppt {height}ppt,"
-            f" move position {x}ppt {y}ppt,"
-            f"{ 'focus' if fetch else ''}"
-        )
+    @functools.lru_cache()
+    def con_rect(self, abs_ref: Rect = None) -> Rect:
+        # relative/ppt
+        if abs_ref is None:
+            width = round(self.shape.x * 100)
+            height = round(self.shape.y * 100)
+            x = {
+                "L": 0,
+                "C": round(50 - (width / 2)),
+                "R": 100 - width,
+            }[self.pos.x]
+            y = {
+                "T": 0,
+                "C": round(50 - (height / 2)),
+                "B": 100 - height,
+            }[self.pos.y]
+        # absolute/px
+        else:
+            width = round(abs_ref.w * self.shape.x)
+            height = round(abs_ref.h * self.shape.y)
+            x = {
+                "L": abs_ref.x,
+                "C": abs_ref.x + round((abs_ref.w / 2) - (width / 2)),
+                "R": abs_ref.x + abs_ref.w - width,
+            }[self.pos.x]
+            y = {
+                "T": abs_ref.y,
+                "C": abs_ref.y + round((abs_ref.h / 2) - (height / 2)),
+                "B": abs_ref.y + abs_ref.h - height,
+            }[self.pos.y]
+        return Rect(x, y, width, height)
 
     def _refresh(self) -> None:
         """Update the information on the presence of the associated Kitty instance,
@@ -258,7 +257,7 @@ class Kitti3:
         tree = self.i3.get_tree()
         if self.con_id is None:
             for con in tree:
-                if self.name in (con.window_instance, con.app_id):
+                if getattr(con, self.client.crit_attr) == self.name:
                     self.con_id = con.id
                     self.con_ws = con.workspace()
                     break
@@ -280,6 +279,68 @@ class Kitti3:
         else:
             self.focused_ws = None
 
+    def align_to_ws(self, context: Trigger) -> None:
+        raise NotImplementedError
+
+
+class Kitts(Kitt):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sway_timing_compat = True  # TODO: add as arg
+
+    def on_moved(self, _, we: i3ipc.WindowEvent) -> None:
+        # Currently under Sway, if a container on an inactive workspace is resized, it
+        # is forcibly reparented to its output's active workspace. Therefore, this
+        # feature is diabled, pending https://github.com/swaywm/sway/issues/6465 .
+        return
+
+    def spawn(self) -> None:
+        r = self.con_rect()
+        self.i3.command(
+            f"for_window [app_id={self.name}] 'floating enable, border none, resize set"
+            f" {r.w}ppt {r.h}ppt, move position {r.x}ppt {r.y}ppt'"
+        )
+        super().spawn()
+
+    def align_to_ws(self, context: Trigger) -> None:
+        if context == Trigger.SPAWNED:
+            return
+        if context == Trigger.FLOATED and self.sway_timing_compat:
+            time.sleep(0.02)
+        r = self.con_rect()
+        crit = f"[con_id={self.con_id}]"
+        resize = f"resize set {r.w}ppt {r.h}ppt"
+        move = f"move position {r.x}ppt {r.y}ppt"
+        if context == Trigger.KEYBIND:
+            fetch = f"move container to workspace {self.focused_ws.name}"
+            cmd = f"{crit} {fetch}, {resize}, {move}, focus"
+        else:
+            cmd = f"{crit} {resize}, {move}"
+        self.i3.command(cmd)
+
+
+class Kitti3(Kitt):
+    def align_to_ws(self, context: Trigger) -> None:
+        # Under i3, in multi-output configurations, a ppt move is considered relative
+        # to the rect defined by the bounding box of all outputs, not by the con's
+        # workspace. Yes, this is madness, and so we have to do absolute moves (and
+        # therefore we also do absolute resizes to stay consistent).
+        if context == Trigger.SPAWNED:
+            # floating will trigger on_floated to do the actual alignment
+            self.i3.command(f"[con_id={self.con_id}] floating enable, border none")
+            return
+        ws = self.focused_ws if context == Trigger.KEYBIND else self.con_ws
+        r = self.con_rect(Rect.from_i3ipc(ws.rect))
+        crit = f"[con_id={self.con_id}]"
+        resize = f"resize set {r.w}px {r.h}px"
+        move = f"move absolute position {r.x}px {r.y}px"
+        if context == Trigger.KEYBIND:
+            fetch = f"move container to workspace {ws.name}"
+            cmd = f"{crit} {fetch}, {resize}, {move}, focus"
+        else:
+            cmd = f"{crit} {resize}, {move}"
+        self.i3.command(cmd)
+
 
 def _split_args(args: List[str]) -> Tuple[List, Optional[List]]:
     try:
@@ -294,7 +355,7 @@ def _simple_fraction(arg: str) -> float:
     if not 0 <= arg <= 1:
         raise argparse.ArgumentError(
             "argument must be a simple fraction, within [0, 1]"
-        )
+    )
     return arg
 
 
@@ -357,13 +418,27 @@ def _parse_args(argv: List[str], defaults: dict) -> argparse.Namespace:
 def cli() -> None:
     argv_kitti3, argv_kitty = _split_args(sys.argv[1:])
     args = _parse_args(argv_kitti3, DEFAULTS)
-    kitti3 = Kitti3(
+
+    conn = i3ipc.Connection()
+    # FIXME: half-baked way of checking what WM we're running on.
+    sway = "sway" in conn.socket_path or conn.get_version().major < 3
+    _Kitt = Kitts if sway else Kitti3
+    # TODO: add arg --client and --crit-attr:
+    #   - if client is single word, create Client from CLIENTS lookup,
+    #   - else ensure name placeholder in client and use w/--crit-attr to create Client
+    #   - client should default to 'kitty'
+    #   - crit-attr should default to window_instance for i3 and app_id for sway
+    client = Client(**CLIENTS["kitty"]["sway" if sway else "i3"])
+
+    kitt = _Kitt(
+        conn=conn,
         name=args.name,
         shape=args.shape,
         pos=args.position,
-        kitty_argv=argv_kitty,
+        client=client,
+        client_argv=argv_kitty,
     )
-    kitti3.loop()
+    kitt.loop()
 
 
 if __name__ == "__main__":
