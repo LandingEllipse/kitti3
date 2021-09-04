6e784171245e316ae3c7400dd091544323a316cb
Ariel Ladegaard <legendin@gmail.com>
2021-09-04 01:21:27 +0100

    build: switch to Poetry

--- src/kitti3/__init__.py
+++ src/kitti3/__init__.py
@@ -1,1 +1,11 @@
-from ._pkg_info import __author__, __homepage__, __version__
+try:
+    import importlib.metadata as importlib_metadata
+except ImportError:
+    import importlib_metadata
+_dist_meta = importlib_metadata.metadata("kitti3")
+__author__ = _dist_meta["Author"]
+__description__ = _dist_meta["Summary"]
+__project__ = _dist_meta["Name"]
+__url__ = _dist_meta["Home-Page"]
+__version__ = _dist_meta["Version"]
+del _dist_meta
